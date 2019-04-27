a51fd5f4bb2d916de43a53c13a60d8a99b3de1c4
nomnombtc <mastergizmo@arcor.de>
2019-04-28 01:35:08 +0200

    checkout specific tag

--- python-trezor/checkout-git.sh
+++ python-trezor/checkout-git.sh
@@ -3,15 +3,17 @@
 TS=$(date +%d%m%y)
 PRGNAM=python-trezor
 URL="https://github.com/trezor/python-trezor.git"
+TAG="v0.11.2"
 
 git clone $URL && \
-	mv $PRGNAM $PRGNAM-git${TS}
+	mv $PRGNAM $PRGNAM-git${TS}_${TAG}
 
-#hmm only useful for udev rules file??
-cd $PRGNAM-git${TS}
+cd $PRGNAM-git${TS}_${TAG}
 git submodule add https://github.com/trezor/trezor-common.git trezor-common
+git fetch --tags
+git checkout $TAG
 cd ..
 
-tar cvjf $PRGNAM-git${TS}.tar.bz2 $PRGNAM-git${TS} && \
-	rm -rf $PRGNAM-git${TS}
+tar cvjf $PRGNAM-git${TS}_${TAG}.tar.bz2 $PRGNAM-git${TS}_${TAG} && \
+	rm -rf $PRGNAM-git${TS}_${TAG}
 
